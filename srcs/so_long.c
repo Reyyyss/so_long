ec5cfb8a741d60a0e8826ea190f83f393845c117
Henrique Carrasquinho <hcr2207@gmail.com>
2025-04-03 14:13:25 +0100

    mais um dia

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hcarrasq <hcarrasq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: henrique-reis <henrique-reis@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 12:07:18 by hcarrasq          #+#    #+#             */
-/*   Updated: 2025/04/02 12:22:20 by hcarrasq         ###   ########.fr       */
+/*   Updated: 2025/04/02 22:57:12 by henrique-re      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 int	main(int ac, char **av)
 {
-	// void	*mlx;
-	// void	*mlx_win;
+	void	*mlx;
+	void	*mlx_win;
 	t_so_long	*slong;
 
-	//initialize_struct();
-	//map_init(&slong.map);
-	/* mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "so_long");
-	mlx_pixel_put(mlx, mlx_win, 128, 128, 0xFF0000);
-	mlx_loop(mlx); */
 
 	if (ac != 2 )//|| ft_strlen(av[1]) < 5)
 		return (write(2, "Error1\n", 6));
@@ -31,5 +25,10 @@ int	main(int ac, char **av)
 		return (write(2, "Error2\n", 6));
 	slong = init_game();
 	map_parsing(av[1], slong->map, slong->ass);
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "so_long");
+	mlx_pixel_put(mlx, mlx_win, 128, 128, 0xFF0000);
+	mlx_loop(mlx);
 }
 
